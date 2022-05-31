68e8e7d9f534d11f246d6f5dc8cea7f4fedc25c0
Dmitry Hotko <d.hotko@invento.by>
2022-05-31 11:01:33 +0300

    tttt polym

---
+++ src/TemperatureConverter/RectangleComponent.java
@@ -0,0 +1,12 @@
+package TemperatureConverter;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RectangleComponent extends JComponent {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawRect(5, 5, getWidth() - 10, getHeight() - 10);
+    }
+}
